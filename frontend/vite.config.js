235b8b42fe5cc1dc7a712f888664441bd720d9a3
Gabriel Mori Francis Abraham <Gabriel.Abraham@ss.zain.com>
2026-01-01 01:01:50 +0200

    Update base to '/' for GitHub Pages with folder /frontend/docs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gpay/',
+  base: '/',
   cacheDir: './node_modules/.vite',
   build: {
     outDir: 'docs'
